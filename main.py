38adbeee1275946c0e77bb3b116132ab7c4e6099
kev0051 <126007369+kev0051@users.noreply.github.com>
2023-11-28 18:45:33 -0600

    Update main.py

--- main.py
+++ main.py
@@ -3,33 +3,18 @@
 # Run the gradebot for project 2: go run main.go project2 -p 8080 -databasefile <database_file_path> -codedir <code_directory_path>
 
 from http.server import BaseHTTPRequestHandler, HTTPServer
-from cryptography.hazmat.primitives import serialization, hashes
+from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives.asymmetric import rsa
-from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from urllib.parse import urlparse, parse_qs
 import base64
 import json
 import jwt
 import datetime
 import sqlite3
-import os
-
-os.environ["NOT_MY_KEY"] = "1"
-
-# Set the encryption key from the environment variable
-encryption_key = os.environ.get('NOT_MY_KEY')
-
-if encryption_key is None:
-    raise ValueError("Environment variable NOT_MY_KEY is not set.")
-
-# Ensure a valid key size for AES (e.g., 16, 24, or 32 bytes)
-valid_key_size = 32  # You can adjust this value based on your security requirements
-
-# Pad or hash the key to achieve the desired length
-encryption_key = (encryption_key * (valid_key_size // len(encryption_key)))[:valid_key_size]
 
 # Create/open SQLite DB file at start
+
 connection = sqlite3.connect('totally_not_my_privateKeys.db')
 
 cursor = connection.cursor()
@@ -50,7 +35,7 @@ cursor.execute('''
         email TEXT UNIQUE,
         date_registered TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
         last_login TIMESTAMP      
-    )
+)
 ''')
 
 # Server code
@@ -67,7 +52,6 @@ expired_key = rsa.generate_private_key(
     key_size=2048,
 )
 
-# Serialize private keys to PEM format
 pem = private_key.private_bytes(
     encoding=serialization.Encoding.PEM,
     format=serialization.PrivateFormat.TraditionalOpenSSL,
@@ -91,14 +75,9 @@ expired_pempkcs1 = expired_key.private_bytes(
     encryption_algorithm=serialization.NoEncryption()
 )
 
-# Encrypt private keys using AES with the provided key
-cipher = Cipher(algorithms.AES(encryption_key.encode('utf-8')), modes.CFB8(os.urandom(16)), backend=default_backend())
-pem_encrypted = cipher.encryptor().update(pem)
-expired_pem_encrypted = cipher.encryptor().update(expired_pem)
-
 # Generate/store at least one key that expires now and one key that expires in 1 hour
-cursor.execute("INSERT INTO keys (kid, key, exp) VALUES (?, ?, ?)", (1, expired_pem_encrypted, datetime.datetime.utcnow()))
-cursor.execute("INSERT INTO keys (kid, key, exp) VALUES (?, ?, ?)", (2, pem_encrypted, datetime.datetime.utcnow() + datetime.timedelta(hours=1)))
+cursor.execute("INSERT INTO keys (kid, key, exp) VALUES (?, ?, ?)", (1, expired_pempkcs1, datetime.datetime.now(datetime.timezone.utc)))
+cursor.execute("INSERT INTO keys (kid, key, exp) VALUES (?, ?, ?)", (2, pempkcs1, datetime.datetime.now(datetime.timezone.utc) + datetime.timedelta(hours=1)))
 
 numbers = private_key.private_numbers()
 
